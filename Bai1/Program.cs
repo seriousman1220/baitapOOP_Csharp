6aac416a1c5a4758c3d2513738e8a246e5a4873d
tuananh603787 <67224723+seriousman1220@users.noreply.github.com>
2021-10-18 20:53:43 +0700

    thêm đối tượng p3 trong program.cs

--- Bai1/Program.cs
+++ Bai1/Program.cs
@@ -10,9 +10,8 @@ namespace Bai1
             DateTime dob = new DateTime(1980, 5, 20);
             Programmer p1 = new Programmer(1,"Nguyen Van A", "Good Communications, Expert in JAVa", dob);
             DateTime dob2 = new DateTime(2001, 1, 1);
-            Programmer p2 = new Programmer(2, "Nguyen Tuan Anh", "Amazing good job", dob2);
-            p1.ShowInfo();
-            p2.ShowInfo();
+            Programmer p3 = new Programmer(2, "Nguyen Tuan Anh", "Good job, amazing,", dob2);
+            p3.ShowInfo();
             Console.ReadKey();
         }
     }
