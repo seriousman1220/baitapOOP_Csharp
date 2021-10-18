82bc9507487b7cabc70baab2c77875f6a3c1c27f
tuananh603787 <67224723+seriousman1220@users.noreply.github.com>
2021-10-18 20:55:11 +0700

    xóa đối tượng p1 trong Program.cs

--- Bai1/Program.cs
+++ Bai1/Program.cs
@@ -7,8 +7,7 @@ namespace Bai1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            DateTime dob = new DateTime(1980, 5, 20);
-            Programmer p1 = new Programmer(1,"Nguyen Van A", "Good Communications, Expert in JAVa", dob);
+            
             DateTime dob2 = new DateTime(2001, 1, 1);
             Programmer p3 = new Programmer(2, "Nguyen Tuan Anh", "Good job, amazing,", dob2);
             p3.ShowInfo();
