6df8a4993eb5aea1b676874a123849e7bf9e6010
tuananh603787 <67224723+seriousman1220@users.noreply.github.com>
2021-10-18 10:02:58 +0700

    thêm interface IPerson và class Employee

---
+++ Bai1/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1
+{
+    abstract class Employee
+    {
+        private int _id;
+        public int ID
+        {
+            get { return _id; }            
+        } 
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value.Length < 3)
+                    throw new Exception("Input value has length less than 3 characters");
+                else
+                    _name = value;
+            }
+        }
+
+        protected Employee(int id)
+        {
+            _id = id;
+            this._name = "No name";
+        }
+
+        protected Employee(int id, string name) : this(id)
+        {
+            _name = name;
+        }
+
+        abstract public void ShowInfo();
+
+
+    }
+}
