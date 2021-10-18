ffa8c4420717a453b08d8247e88a199a92d0ccdd
tuananh603787 <67224723+seriousman1220@users.noreply.github.com>
2021-10-18 10:34:37 +0700

    Thêm đối tượng p1 vào phần test

--- Bai1/Program.cs
+++ Bai1/Program.cs
@@ -7,6 +7,11 @@ namespace Bai1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            DateTime dob = new DateTime(1980, 5, 20);
+            Programmer p1 = new Programmer(1,"Nguyen Van A", "Good Communications, Expert in JAVa", dob);
+
+            p1.ShowInfo();
+            Console.ReadKey();
         }
     }
 }
