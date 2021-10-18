2c4b0d0c67c30b96868d84283d19e082674862f9
tuananh603787 <67224723+seriousman1220@users.noreply.github.com>
2021-10-18 18:54:35 +0700

    thêm đối tượng p2

--- Bai1/Program.cs
+++ Bai1/Program.cs
@@ -9,8 +9,10 @@ namespace Bai1
             Console.WriteLine("Hello World!");
             DateTime dob = new DateTime(1980, 5, 20);
             Programmer p1 = new Programmer(1,"Nguyen Van A", "Good Communications, Expert in JAVa", dob);
-
+            DateTime dob2 = new DateTime(2001, 1, 1);
+            Programmer p2 = new Programmer(2, "Nguyen Tuan Anh", "Amazing good job", dob2);
             p1.ShowInfo();
+            p2.ShowInfo();
             Console.ReadKey();
         }
     }
